334b6be8971da27007b595de3c758072bcca76d4
Trideb <panigrahitrideb1@gmail.com>
2026-04-08 20:11:52 +0530

    Added frequency

---
+++ April2026/8thApril2026/frequencyCheck.js
@@ -0,0 +1,9 @@
+function frequencyCheck(str) {
+  let obj = {};
+  for (let ele of str) {
+    obj[ele] = (obj[ele] || 0) + 1;
+  }
+  return obj;
+}
+
+console.log(frequencyCheck("kjhfjaheuyewiy"))
