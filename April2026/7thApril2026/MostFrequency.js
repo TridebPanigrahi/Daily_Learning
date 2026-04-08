44fce6cf4fc8b681fcce6b1e6f161e7a0e20ef94
Trideb <panigrahitrideb1@gmail.com>
2026-04-08 19:59:34 +0530

    Fixed issues

--- April2026/7thApril2026/MostFrequency.js
+++ April2026/7thApril2026/MostFrequency.js
@@ -1,7 +1,15 @@
 function frequency(arr, k) {
   let obj = {};
   for (let ele of arr) {
-    obj[ele] = obj[ele] || 0 + 1;
+    obj[ele] = (obj[ele] || 0) + 1;
   }
-  
+  const sortedObj = Object.fromEntries(
+    Object.entries(obj)
+      .sort((a, b) => b[1] - a[1])
+      .slice(0, k),
+  );
+  return Object.keys(sortedObj);
+  // 8653535631;
 }
+
+console.log(frequency([1, 1, 1, 2, 3, 4, 5, 5], 2));
