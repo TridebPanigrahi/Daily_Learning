a0e4a5a92bd777a37293cf74d1c2099e9e6ffaba
Trideb <panigrahitrideb1@gmail.com>
2026-04-13 20:34:21 +0530

    Added Duplicate Sorted

---
+++ April2026/13thApril2026/duplicateSortedArr.js
@@ -0,0 +1,18 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const removeDuplicate = (head) => {
+  let curr = head;
+  while (curr && curr.next) {
+    if (curr.val === curr.next.val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+  return head;
+};
