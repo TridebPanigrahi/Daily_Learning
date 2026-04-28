68e821617553b0388d408c528d2c4af015a99a84
Trideb <panigrahitrideb1@gmail.com>
2026-04-28 21:08:07 +0530

    alternativeArray

---
+++ April2026/28thApril2026/alternativeArray.js
@@ -0,0 +1,34 @@
+// You are given an array arr[], the task is to return a list elements of arr in alternate order (starting from index 0).
+
+// Examples:
+
+// Input: arr[] = [1, 2, 3, 4]
+// Output: 1 3
+// Explanation:
+// Take first element: 1
+// Skip second element: 2
+// Take third element: 3
+// Skip fourth element: 4
+// Input: arr[] = [1, 2, 3, 4, 5]
+// Output: 1 3 5
+// Explanation:
+// Take first element: 1
+// Skip second element: 2
+// Take third element: 3
+// Skip fourth element: 4
+// Take fifth element: 5
+// Constraints:
+// 1 <=  arr.size <= 105
+// 1 <= arr[i] <= 105
+
+function alternativeArray(arr){
+    let result = []
+    for(let i=0;i<arr.length;i++){
+        if(i%2 ===0){
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+
+console.log(alternativeArray([1, 2, 3, 4]))
